0f3aef51cc37854c706a506c57c2f21df2f8dda3
Enoch Phetteplace <curiousaps@gmail.com>
2025-01-24 12:07:00 -0600

    Hilt setup and new icon

--- app/src/main/java/com/curiousapps/bambupractice/MainActivity.kt
+++ app/src/main/java/com/curiousapps/bambupractice/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.curiousapps.bambupractice.ui.theme.BambuPracticeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +23,7 @@ class MainActivity : ComponentActivity() {
             BambuPracticeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "New Bambu",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
